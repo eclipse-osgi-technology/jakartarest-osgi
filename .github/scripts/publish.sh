7deccdf1608c488a7307c57888a5beb58559efb5
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2023-06-22 19:08:55 +0200

    Add Maven wrapper
    - added Maven wrapper
    
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

---
+++ .github/scripts/publish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#*********************************************************************
+# Copyright (c) 2023-06-22 Data In Motion Consulting and others
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#**********************************************************************
+set -ev
+
+./mvnw --batch-mode deploy "$@"
