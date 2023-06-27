77a4a3f9db710ced565990fe16e6881dd46b7fe4
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2023-06-27 11:01:19 +0200

    Use mvnw for Dash
    - use mvnw for dash ip check
    
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

---
+++ .github/scripts/ip-check.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#*********************************************************************
+# Copyright (c) 2023-06-22 Data In Motion Consulting and others
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#**********************************************************************
+set -ev
+
+./mvnw --batch-mode --version
+./mvnw --batch-mode --no-transfer-progress -B org.eclipse.dash:license-tool-plugin:license-check -Dlicenses-check -Ddash.fail=true -Ddash.summary=DEPENDENCIES -DexcludeArtifactIds=$EXCLUDED_ARTIFACTS -Peclipse-licenses-check "$@"
